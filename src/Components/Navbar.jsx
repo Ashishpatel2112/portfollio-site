476d8d73772e35880a128c8947e6854244b8b83c
ASHISH PATIDAR <110155660+Ashishpatel2112@users.noreply.github.com>
2025-05-13 16:26:17 +0530

    first commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { HiOutlineBars3CenterLeft } from 'react-icons/hi2'
+
+const Navbar = () => {
+  return (
+    <>
+    <div className="navbar border h-[50px]">
+<HiOutlineBars3CenterLeft className=' text-3xl' />
+
+    </div>
+    </>
+  )
+}
+
+export default Navbar
